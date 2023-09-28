d987a8483eef2b80b08c5cb354e017594eaf20c7
Luis Ceren <luiscerenc18@gmail.com>
2023-09-28 14:49:35 -0500

    Functional components -done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
 import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 
 function App() {
-  const handleNameChange = () => {
-    const names = ["Bob", "Kevin", "Luis", "Dave"];
-    const int = Math.floor(Math.random() * 4);
-    return names[int];
-  };
   return (
     <div className="App">
       <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
