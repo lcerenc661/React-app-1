fd142c27ac6c5d2c24f5108482e862e042f24ddb
Luis Ceren <luiscerenc18@gmail.com>
2023-10-02 15:06:20 -0500

    09 - Controlled components inputs - Done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
+import AddItem from "./AddItem";
+import SearchItem from "./SearchItem";
 
 import { useState } from "react";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound of Cocoa covered Almnonds Unsalted",
-    },
-    { id: 2, checked: false, item: "Item 2" },
-    { id: 3, checked: false, item: "Item 3" },
-  ]);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shoppinglist"))
+  );
+
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+
+  const setAndSaveItems = (newListItems) => {
+    setItems(newListItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(newListItems));
+  };
+
+  const addNewItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    console.log(newItem);
+    //add Item
+    addNewItem(newItem);
+    setNewItem("");
+    console.log("submitted");
   };
   return (
     <div className="App">
       <Header title="Groceries List" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
       <Content
-        items={items}
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
