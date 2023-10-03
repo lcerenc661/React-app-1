6a4253263f43bf9bc0d657ce8614f265ed7e7346
Luis Ceren <luiscerenc18@gmail.com>
2023-10-03 15:33:09 -0500

    11 - Fetch API Data - Done

--- src/App.js
+++ src/App.js
@@ -7,17 +7,33 @@ import SearchItem from "./SearchItem";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shoppinglist")) || []
-  );
-
+  const API_URL = "http://localhost:3500/items";
+  const [items, setItems] = useState( []);
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoadiang] = useState(true);
 
   // If the dependecy changes [], the arrow function excecutes
   useEffect(() => {
-    localStorage.setItem("shoppinglist", JSON.stringify(items));
-  }, [items]);
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL)
+        if (!response.ok) throw Error('Did not recived expected data');
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err){
+        console.log(err.stack);
+        setFetchError(err.message);
+      } finally {
+        setIsLoadiang(false)
+      } 
+    }
+    setTimeout(() => {
+      (async () => await fetchItems())();
+    }, 2000);
+  }, []);
 
   const addNewItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -56,6 +72,10 @@ function App() {
         handleSubmit={handleSubmit}
       />
       <SearchItem search={search} setSearch={setSearch} />
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{color: "red"}}>{`Error: ${fetchError}`} </p>}
+        {!fetchError && !isLoading &&
       <Content
         items={items.filter((item) =>
           item.item.toLowerCase().includes(search.toLowerCase())
@@ -63,6 +83,8 @@ function App() {
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
+        }
+      </main>
       <Footer length={items.length} />
     </div>
   );
--- src/Content.js
+++ src/Content.js
@@ -4,13 +4,13 @@ import ItemList from "./ItemList";
 const Content = ({items, handleCheck, handleDelete}) => {
 
   return (
-    <main>
+    <>
       {items.length ? (
         <ItemList items={items} handleCheck={handleCheck} handleDelete={handleDelete} />
       )
       : <p style={{marginTOP: '2rem'}}>Empty List.</p>
       }
-    </main>
+    </>
   );
 };
 
